4646f9e7bf0be21428e51667bf6ba5ed8ff18842
Sean Rankine <sean.rankine@me.com>
2018-10-08 20:18:27 +0100

    Chapter 7: Diffuse Materials

---
+++ src/hitable.hpp
@@ -0,0 +1,17 @@
+#ifndef HITABLE
+#define HITABLE
+
+#include "ray.hpp"
+
+struct hit_record {
+  float t;
+  vec3 p;
+  vec3 normal;
+};
+
+class hitable {
+public:
+  virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
-#include "ray.hpp"
+#include "sphere.hpp"
+#include "hitable_list.hpp"
+#include "camera.hpp"
+#include "float.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius*radius;
-	float discriminant = b*b - 4*a*c;
-	if (discriminant < 0){
-		return -1.0;
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+
+vec3 color(const ray& r, hitable *world){
+	hit_record rec;
+	if (world->hit(r, 0.00001, MAXFLOAT, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world); 
 	} else {
-		return (-b - sqrt(discriminant)) / (2.0 * a);
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
 }
 
-vec3 color(const ray& r){
-	float t = hit_sphere(vec3(0,0,-1), 0.5, r);
-	if (t > 0.0){
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
-		return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-	}
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-}
+// float rand_f(){
+// 	return (rand() / (RAND_MAX)) + 1;
+// }
 
 int main(){
-	int factor = 12;
+	int factor = 3;
 	int nx = 200 * factor;
 	int ny = 100 * factor;
+	int ns = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+	hitable *list[2];
+	list[0] = new sphere(vec3(0,0,-1), 0.5);
+	list[1] = new sphere(vec3(0,-100.5,-1), 100);
+	hitable *world = new hitable_list(list, 2);
+	camera cam;
 
 	for (int j = ny-1; j >= 0; j--){
 		for (int i = 0; i < nx; i++){
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col = color(r);
+			vec3 col(0,0,0);
+			for(int s = 0; s < ns; s++){
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
